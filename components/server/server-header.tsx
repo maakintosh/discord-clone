411e252307d7e3e75d9da7e346441c52d418c42c
tosh404 <maquintosh404@gmail.com>
2023-09-20 12:42:47 +0900

    feat: display server-header pulldown menu

---
+++ components/server/server-header.tsx
@@ -0,0 +1,86 @@
+'use client'
+
+import { ServerWithMembersWithProfile } from '@/type'
+import { MemberRole } from '@prisma/client'
+import {
+  ChevronDown,
+  Hash,
+  LogOut,
+  Settings,
+  Trash2,
+  UserPlus2,
+  Users2,
+} from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+interface ServerHeaderProps {
+  server: ServerWithMembersWithProfile
+  role?: MemberRole
+}
+
+export function ServerHeader({ server, role }: ServerHeaderProps) {
+  const isAdmin = role === MemberRole.ADMIN
+  const isModerator = isAdmin || role === MemberRole.MODERATOR
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="secondary"
+          className="text-md flex h-12 w-full items-center px-3 font-semibold"
+        >
+          {server.name}
+          <ChevronDown className="ml-auto h-5 w-5" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56 space-y-1 font-medium text-black dark:text-zinc-300">
+        {isModerator && (
+          <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400">
+            Invite People
+            <UserPlus2 className="ml-auto h-5 w-5" />
+          </DropdownMenuItem>
+        )}
+        {isModerator && (
+          <DropdownMenuItem className="">
+            Create Channel
+            <Hash className="ml-auto h-5 w-5" />
+          </DropdownMenuItem>
+        )}
+        <DropdownMenuSeparator />
+        {isAdmin && (
+          <DropdownMenuItem className="">
+            Manage Members
+            <Users2 className="ml-auto h-5 w-5" />
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem className="">
+            Server Setttings
+            <Settings className="ml-auto h-5 w-5" />
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem className="text-rose-600 dark:text-rose-400">
+            Delete Server
+            <Trash2 className="ml-auto h-5 w-5" />
+          </DropdownMenuItem>
+        )}
+        <DropdownMenuSeparator />
+        {!isModerator && (
+          <DropdownMenuItem className="text-rose-600 dark:text-rose-400">
+            Leave Server
+            <LogOut className="ml-auto h-5 w-5" />
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
