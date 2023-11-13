351fcbf9ba0e157c8e6f07f3bde1e22f4947f047
tosh404 <102001127+tosh404@users.noreply.github.com>
2023-11-13 18:00:54 +0900

    chore: update comment

--- pages/api/socket/messages/index.ts
+++ pages/api/socket/messages/index.ts
@@ -61,6 +61,7 @@ export default async function handler(
       },
     })
 
+    // this 'channelKey' should be identical string to the one passed into use-chat-socket (addKey in this case).
     const channelKey = `chat:${channelId}:messages`
 
     // the Socket.IO library, which enables real-time, bidirectional, and event-based communication between the browser and the server .The emit function is used to send messages from the server to the client.
