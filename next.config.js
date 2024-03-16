84bb8fbad3a0875dd600238004a5ca9d77b54f77
tosh404 <maquintosh404@gmail.com>
2024-03-16 16:21:35 +0000

    build: updated devcontainer files to adapt dood, which enabled to use docker commands inside devcontianer

--- next.config.js
+++ next.config.js
@@ -9,10 +9,10 @@ const nextConfig = {
   },
   images: {
     domains: ['uploadthing.com', 'utfs.io']
-  },
+  }
   // This setting will create a folder at .next/standalone that copies only the necessary files for a production deployment and can then be deployed on its own without installing node_modules.
   // More info: https://nextjs.org/docs/app/api-reference/next-config-js/output#automatically-copying-traced-files
-  output: 'standalone'
+  // output: 'standalone'
 }
 
 module.exports = nextConfig
